663a13ffa47e4347023b91cc5e480b60faef1114
Brian Seegmiller <brianseegmiller@gmail.com>
2025-09-17 09:07:06 -0700

    changed the way google ad sense is loaded in page.

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,11 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <Script src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1050367821834787" async crossOrigin="anonymous"></Script>
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1050367821834787"
+          strategy='lazyOnload'
+          crossOrigin="anonymous"></Script>
       </head>
       <GoogleTagManager gtmId="GTM-W7VZXWBZ" />
       <body className={`${exo2.variable} ${spaceMono.variable} antialiased`}>
